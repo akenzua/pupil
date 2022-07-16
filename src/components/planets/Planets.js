f8f389aa6ddacd26f6f922036faab062f6a00df8
Akintade Ajibade <ajibade.akintade@gmail.com>
2022-07-16 23:02:12 +0100

    created planets component

--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,12 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Planets = () => {
+  const planetsList = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+  ];
+
+  const displayPlanet = planetsList.map((planetItem, index) => {
+    return (
+      <li key={index}>
+        <Link to={`/${planetItem.toLowerCase()}`}>{planetItem}</Link>
+      </li>
+    );
+  });
+
   return (
     <>
-      <p>Planets</p>
-      <aside>
+      <ul>{displayPlanet}</ul>
+      <section>
         <Outlet />
-      </aside>
+      </section>
     </>
   );
 };
