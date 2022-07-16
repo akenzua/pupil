7139281af56556d1d8a0f47b706c7f1efa3ad170
Akintade Ajibade <ajibade.akintade@gmail.com>
2022-07-16 22:58:07 +0100

    test planets component

---
+++ src/components/planets/Planets.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Planets from "./";
+
+describe("Planets", () => {
+  const MockPlanets = () => {
+    return (
+      <BrowserRouter>
+        <Planets />
+      </BrowserRouter>
+    );
+  };
+  it("should render all planets links", () => {
+    render(<MockPlanets />);
+    expect(screen.getByText(/mercury/i)).toBeInTheDocument();
+    expect(screen.getByText(/venus/i)).toBeInTheDocument();
+    expect(screen.getByText(/earth/i)).toBeInTheDocument();
+    expect(screen.getByText(/mars/i)).toBeInTheDocument();
+    expect(screen.getByText(/jupiter/i)).toBeInTheDocument();
+    expect(screen.getByText(/saturn/i)).toBeInTheDocument();
+    expect(screen.getByText(/uranus/i)).toBeInTheDocument();
+    expect(screen.getByText(/neptune/i)).toBeInTheDocument();
+  });
+});
