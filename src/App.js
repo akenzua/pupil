1e3bbb02ab055b33c3fb2c3b3d7e959330169ceb
Akintade Ajibade <ajibade.akintade@gmail.com>
2022-07-16 22:56:05 +0100

    setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Planets from "./components/planets";
+import PlanetDetails from "./components/planetDetails";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Planetry Bodies</h1>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Planets />}>
+              <Route path="/:planet" element={<PlanetDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 const Planets = () => {
-  return <p>Planets</p>;
+  return (
+    <>
+      <p>Planets</p>
+      <aside>
+        <Outlet />
+      </aside>
+    </>
+  );
 };
 
 export default Planets;
